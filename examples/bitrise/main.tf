7086e7f9a3e66ad8c98df05bcdef56f6e8445616
Nate Schlie <schlie.nt@pg.com>
2023-04-19 15:37:37 -0400

    flutter

--- examples/bitrise/main.tf
+++ examples/bitrise/main.tf
@@ -13,8 +13,8 @@ resource "bitrise_app" "app" {
   token         = "EZgewzA9KET4uj4cFqoadeLiHwBMKV4orgmZ7kd3AGy_yiMKGBPt050u7KT7fFRd7otH3KGuDKBeftVj0pCxkw"
   repo_url      = "https://github.com/pgdevelopers/nates_bitrise_provider_app.git"
   git_repo_slug = "nates_bitrise_provider_app"
-  title         = "nates-cool-sanity-check"
-  project_type  = "ios"
-  stack_id      = "osx-xcode-13.2.x"
-  config        = "default-ios-config"
+  title         = "nates-cool-flutter-again"
+  project_type  = "flutter"
+  stack_id      = "osx-xcode-14.2.x-ventura"
+  config        = "flutter-config-test-app-both"
 }
