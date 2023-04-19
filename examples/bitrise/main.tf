dbb788c07331d0081e64a979e6b60f920360cb90
Nate Schlie <schlie.nt@pg.com>
2023-04-19 11:52:17 -0400

    hmmm

--- examples/bitrise/main.tf
+++ examples/bitrise/main.tf
@@ -14,7 +14,7 @@ provider "bitrise" {
 resource "bitrise_app" app {
   repo_url          = "https://github.com/pgdevelopers/nates_bitrise_provider_app.git"
   git_repo_slug     = "nates_bitrise_provider_app"
-  title             = "made-by-provider-wow"
+  title             = "nates-cool-app"
   project_type      = "ios"
   stack_id          = "osx-xcode-13.2.x"
   config            = "default-ios-config"
